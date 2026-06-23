bf7c7a284e49aadbfb256779e5f9cf8ab5229941
Pichaya <impitch.phanupong@gmail.com>
2026-06-23 11:45:12 +0700

    auto: 11:45:12

--- Project1/main.c
+++ Project1/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() 
 {
+	int number1 = 10;
+	int number2 = 3;
+	printf("%d + %d = %d\n",number1,number2,number1+number2);
+	printf("%d - %d = %d\n", number1, number2, number1 - number2);
+	printf("%d * %d = %d\n", number1, number2, number1 * number2);
 	return 0;
 }
